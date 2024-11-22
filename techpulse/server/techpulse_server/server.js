fd7cd9986b5984fa0e948e94918767ff7f89b38f
miruomoo <mlin282@uwo.ca>
2024-11-21 19:04:43 -0500

    Update server.js

--- techpulse/server/techpulse_server/server.js
+++ techpulse/server/techpulse_server/server.js
@@ -1,10 +1,36 @@
 const OpenAI = require('openai');
 const dontenv = require("dotenv");
+
 dontenv.config();
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.json());
+
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+app.listen(4000, () => {
+    console.log("Server is running on port 4000")
+})
+
+//Server APIs
+app.post("/gpt", async (req, res) => {
+    let prompt = req.body.prompt;
+    let response = await promptAI(prompt);
+    res.send(response);
+})
 
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
+
+//Prompt function
 
 let promptAI = async (prompt) => {
 const response = await openai.chat.completions.create({
